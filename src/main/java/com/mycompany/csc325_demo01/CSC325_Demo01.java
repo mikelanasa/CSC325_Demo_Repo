eba9f58fd6de93ca67eb8eeff6ea586196b3eaa4
mikelanasa <mikelanasa@gmail.com>
2023-02-01 17:24:23 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/csc325_demo01/CSC325_Demo01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csc325_demo01;
+
+/**
+ *
+ * @author mikel
+ */
+public class CSC325_Demo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Mike");
+    }
+}
